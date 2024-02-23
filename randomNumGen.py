e6eedbb94a529e22b442e1c237df954784bee6ef
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-02-24 01:57:24 +0200

    Updating full code

--- randomNumGen.py
+++ randomNumGen.py
@@ -1,2 +1,36 @@
-print("lks")
-op=input()
+#Random Number Generator
+
+import random
+
+top_range=input("Type your highest number range")
+if top_range.isdigit():
+    top_range=int(top_range)
+
+    if top_range<=0:
+        print("Please try a number greater than 0")
+        quit()
+else:
+    print("Please Enter a number not a letter")
+    quit()
+
+random_num = random.randint(0,top_range)
+guesses=0
+
+while True:
+    guesses+=1
+    userInput=input("Guess a number")
+    if userInput.isdigit():
+        userInput=int(userInput)
+    else:
+        print("Please type a number")
+        continue
+    if userInput==random_num:
+        print("You got it!!!")
+        break
+    
+    elif userInput>random_num:
+            print("You are above of number")
+    else:
+            print("You are below number")
+
+print("You got it in", guesses, "guesses")
