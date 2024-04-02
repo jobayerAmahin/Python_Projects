64766f6814a86c4b9455f876e55b6547ccc8e796
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-04-02 23:56:15 +0300

    Uploading Finland Immigrants conversation update on integration program

--- FinnComers.py
+++ FinnComers.py
@@ -9,7 +9,12 @@ if categ=="A":
     elif purA=="Spouse":
         tar=input("Are you a job seeker?Y/N")
         if tar=="Y":
-            print("Immediately contact with TE service")
+            print("Immediately contact to TE office")
+            train=input("What are you planing to learn? 1. Language. 2.Professional Traing? Please type 1 or 2")
+            if train=="1":
+                print("You have to wait a few months as the queue is long")
+            else:
+                print("We will send you some link of Work Trial through your email")
         else:
             print("We hope a good plan from you about your reason of residency")
     else:
